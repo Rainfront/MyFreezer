fc21ac954fb172188161dc50c87342d364612899
Rainfront <Chudoha@gmail.com>
2023-10-08 17:59:40 +0300

    Changed react project

---
+++ myfreezer.client/src/Components/AppContent.tsx
@@ -0,0 +1,9 @@
+
+import {Button} from "@mui/material"
+
+export default function AppContent(){
+    
+    return (<>
+        <Button variant={"outlined"} color={"success"}>Text</Button>
+    </>)
+}
